a9498b682a6696006ebf9e7b418c83a4c3a5a89f
DiegoSF9009 <soldadodecolorrosa@gmail.com>
2026-06-16 20:31:27 -0600

    Antes del parcial
    sonidos

--- Assets/Scrips/EnemyFollow.cs
+++ Assets/Scrips/EnemyFollow.cs
@@ -1,58 +1,72 @@
 using UnityEngine;
 using System.Collections;
 
-public class EnemyFollow : MonoBehaviour
+public class EnemyFollow : Enemy ///*MonoBehaviour ///*Agregando Enemy
 {
     [SerializeField]
     private float speed = 3f;
     [SerializeField]
-    private float yPosition = 2f;
+    private float yPosition = 2f;       ///*BORRANDO COSAS
     ///* animaciones enemigo
-    [SerializeField]
-    private float damage = 20f;
+    ///* [SerializeField]
+    ///*private float damage = 20f;
     [SerializeField]
     private float pushForce = 5f;
     ///* animaciones enemigo
-    private Transform player;
+    ///*private Transform player;
     private bool isFollowing = true;
-    private Animator animator;
-    private void Start()
+    ///*private Animator animator;
+    ///*private void Start()
+    ///*{
+        ///*animator = GetComponent<Animator>();
+        ///*GetComponent<Health>().InitializeHealth(); ///*sFalto este
+    ///*}
+    ///* CREANDO UNA NUEVA
+    public override void OnEnable()
     {
-        animator = GetComponent<Animator>();
-        GetComponent<Health>().InitializeHealth(); ///*sFalto este
+        base.OnEnable();
+        animator.Play("Appear", 0, 0f); ///* Aparecer con sonido
+        isFollowing = true; ///* Agregando el nombre de sonido
+        SoundManager. instance.Play("Takopi_appear"); ///*Aparecer
     }
-    private void OnEnable()
-    {
-        player = GameObject.FindGameObjectWithTag("Player").transform;
-    }
-    public void TakeDamage()
-    {
+    public override void TakeDamage()
+    {                           ///* Agregando el nombre de sonido
+        SoundManager. instance.Play("Takopi_damage"); ///* Daño
         if (!isFollowing) return;
         isFollowing = false;
-        animator.Play("Damage", 0, 0f);
+        base. TakeDamage();
         StartCoroutine(StopAndFollow());
     }
+    ///* CONSERVANDO ESTA
+    ///*private void OnEnable()
+    ///*{
+        ///*player = GameObject.FindGameObjectWithTag("Player").transform;
+    ///*}
+    ///* CONSERVANDO ESTA
     private IEnumerator StopAndFollow()
     {
         yield return null;
         yield return new WaitForSeconds(animator.GetCurrentAnimatorStateInfo(0).length);
         isFollowing = true;
     }
+    ///* CAMBIANDO CASI ESTA
     public void Die()
     {
-        StopAllCoroutines();
-        GetComponent<Collider>().enabled = false; ///* para que haga la animacion
+        ///*StopAllCoroutines(); ///* Agregando el nombre de sonido
+        SoundManager.instance.Play("Takopi_die"); ///* muere el enemigo
+        ///*GetComponent<Collider>().enabled = false; ///* para que haga la animacion
         isFollowing = false;
-        animator.Play("Death", 0, 0f);
-        StartCoroutine(DieCoroutine());
+        ///*animator.Play("Death", 0, 0f);
+        ///*StartCoroutine(DieCoroutine());
+        base.Die(); ///* Agregando esta
     }
-    private IEnumerator DieCoroutine()
-    {
-        yield return null;
-        yield return new WaitForSeconds(animator.GetCurrentAnimatorStateInfo(0).length);
-        Destroy(gameObject);
-    }
-    private void Update()
+    ///*private IEnumerator DieCoroutine()
+    ///*{
+        ///*yield return null;
+        ///*yield return new WaitForSeconds(animator.GetCurrentAnimatorStateInfo(0).length);
+        ///*Destroy(gameObject);
+    ///*}
+    private void Update()   ///*DEJAR ESTA COMO ESTA
     {
         if (!isFollowing) return;
         Vector3 targetPosition = new Vector3(player.position.x, yPosition, player.position.z);
@@ -63,7 +77,8 @@ public class EnemyFollow : MonoBehaviour
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.CompareTag("Player"))
-        {
+        {                                          ///* muere el enemigo
+            SoundManager.instance.Play("cacodemon_attack"); ///* Ataque
             collision.gameObject.GetComponent<Player>().PushBack(transform, pushForce);
             collision.gameObject.GetComponent<Health>().TakeDamage(damage);
         }
